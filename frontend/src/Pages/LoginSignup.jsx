4e89094f0a025134e5c8b25eaf0a366728747561
chiranjali <akachiranjali23@gmail.com>
2025-07-18 23:03:11 +0530

    LoginPage

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import './CSS/loginsignup.css'
+
 
 export default function LoginSignup() {
   return (
-    <div>
-      <h1>Login </h1>
+    <div className='login-signup'>
+      <div className="loginsignup-container">
+        <h1>sign up</h1>
+        <div className="loginsignup-field">
+          <input type="text" placeholder='Enter your name' />
+          <input type="email" placeholder='Enter your email' />
+          <input type="password" placeholder='Enter your password' />
+        </div>
+        <button className='loginsignup-btn'>Sign Up</button>
+        <p className='loginsignup-login'>Already have an account? <span>Login</span></p>
+        
+        <div className="loginsignup-agree">
+          <input type="checkbox" />
+          <p>I agree to the <span>Terms & Conditions</span> and <span>Privacy Policy</span></p>
+
+        </div>
+      </div>
+      
     </div>
   )
 }
