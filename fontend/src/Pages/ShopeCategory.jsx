917d18f80bb0dc5d48103ed6dcc973340001a974
chiranjali <akachiranjali23@gmail.com>
2025-07-20 08:26:03 +0530

    Add backend folder and update frontend

--- fontend/src/Pages/ShopeCategory.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ShopeCategory() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
--- frontend/src/Components/NavBar/NavBar.js
+++ frontend/src/Components/NavBar/NavBar.js
@@ -1,37 +1,43 @@
-import React, { useState, useContext } from 'react'; 
+import React, { useState, useContext, useRef } from 'react'; 
 import '../NavBar/NavBar.css';
 import logo from '../../Components/Assest/logo.png';
 import cart_icon from '../../Components/Assest/cart_icon.png';
 import { Link } from 'react-router-dom';
 import { ShopContext } from '../../Context/ShopeContext'; 
 
+
 export default function NavBar() {
   const [menu, setMenu] = useState("shop");
   const { getTotalCartItems } = useContext(ShopContext);
+  const menuRef = useRef();
+
+ 
 
   return (
     <div className='navbar'>
       <div className='navbar__logo'>
-        <img src={logo} alt='' />
+        <img src={logo} alt='logo' />
         <p>SHOPPER</p>
       </div>
 
-      <ul className='navbar_menu'>
+      
+
+      <ul ref={menuRef} className='navbar_menu'>
         <li onClick={() => setMenu("Shop")}>
           <Link to='/'>Shop</Link>
-          {menu === "Shop" ? <hr /> : null}
+          {menu === "Shop" && <hr />}
         </li>
         <li onClick={() => setMenu("Mens")}>
           <Link to='/mens'>Men</Link>
-          {menu === "Mens" ? <hr /> : null}
+          {menu === "Mens" && <hr />}
         </li>
         <li onClick={() => setMenu("Womens")}>
           <Link to='/womens'>Women</Link>
-          {menu === "Womens" ? <hr /> : null}
+          {menu === "Womens" && <hr />}
         </li>
         <li onClick={() => setMenu("Kids")}>
           <Link to='/kids'>Kids</Link>
-          {menu === "Kids" ? <hr /> : null}
+          {menu === "Kids" && <hr />}
         </li>
       </ul>
 
@@ -39,10 +45,11 @@ export default function NavBar() {
         <Link to='/login'>
           <button>Login</button>
         </Link>
-        <Link to='/cart'>
-          <img src={cart_icon} alt='' />
+
+        <Link to='/cart' className='cart-icon-link'>
+          <img src={cart_icon} alt='cart' />
+          <div className='add_cart_counter'>{getTotalCartItems()}</div>
         </Link>
-        <div className='add_cart_counter'>{getTotalCartItems()}</div>
       </div>
     </div>
   );
